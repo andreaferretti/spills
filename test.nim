61e78698d0adb17768940702847e5ebbff7ce175
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-06-06 23:40:21 +0200

    Added a few tests

--- test.nim
+++ test.nim
@@ -1,57 +1,62 @@
-import spills, spills/varchar
+import unittest, spills, spills/varchar
 
 type Foo = object
   a, b: int
   c: float
-  d: VarChar[10]
 
 initSpills()
 
-var x = writableSpill[Foo]()
-for i in 0 .. 1000000:
-  x.add(Foo(a: i, b: i + 1, c: i.float, d: ($i).varchar(10)))
-x.close()
+suite "spills":
+  var x = writableSpill[Foo]()
+  for i in 0 .. 100:
+    x.add(Foo(a: i, b: i + 1, c: i.float))
+  x.close()
 
-var y = spill(x)
-echo y
-echo y[1234]
+  test "spill creation":
+    var y = spill(x)
+    check y[10] == Foo(a: 10, b: 11, c: 10.0)
+    y.close()
+  test "map operation":
+    var
+      y = spill(x)
+      z = y.map(proc(f: Foo): float = f.c)
+    check z[25] == 25.0
+    y.close()
+    z.close()
+  test "filter operation":
+    var
+      y = spill(x)
+      z = y.filter(proc(f: Foo): bool = f.a mod 2 == 0)
+    check z[25] == Foo(a: 50, b: 51, c: 50.0)
+    y.close()
+    z.close()
+  test "length operation":
+    var
+      y = spill(x)
+      z = y.filter(proc(f: Foo): bool = f.a mod 2 == 0)
+    check y.len == 101
+    check z.len == 51
+    y.close()
+    z.close()
+  test "fold operations":
+    var
+      y = spill(x)
+      z = y.map(proc(f: Foo): float = f.c)
+      s1 = foldl(z, a + b)
+      s2 = foldr(z, a + b)
+    check s1 == 5050.0
+    check s2 == 5050.0
+    y.close()
+    z.close()
+  test "seq conversions":
+    var
+      s = @[
+        Foo(a: 2, b: 5, c: 3.14),
+        Foo(a: 3, b: 2, c: 1.0)
+      ]
+      t = s.toSpill()
+      u = t.toSeq()
 
-var z1 = y.map(proc(f: Foo): float = f.c)
-
-echo z1[1234]
-
-var z2 = y.filter(proc(f: Foo): bool = f.a mod 2 == 0)
-
-echo len(z2)
-
-echo z2[1234]
-
-let sum1 = foldl(z1, a + b)
-echo sum1
-let sum2 = foldr(z1, a + b)
-echo sum2
-
-y.close()
-z1.close()
-z2.close()
-
-var a = writableSpill[Foo]()
-a.close()
-var b = spill(a)
-echo b
-b.close()
-
-var
-  s = @[
-    Foo(a: 2, b: 5, c: 3.14, d: "Hello".varchar(10)),
-    Foo(a: 3, b: 2, c: 1.0, d: "World".varchar(10))
-  ]
-  t = s.toSpill()
-  u = t.toSeq()
-
-echo t
-echo u
-
-t.close()
+    check s == u
 
 destroySpills()
